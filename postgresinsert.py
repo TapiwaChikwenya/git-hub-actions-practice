cc96bc118423df473238e2aba17ce0c74e26778c
tapiwa <tapiwachikwenya@gmail.com>
2023-06-19 23:19:15 -0500

    fixing pylint errors

--- postgresinsert.py
+++ postgresinsert.py
@@ -1,13 +1,22 @@
+"""
+This module connects to the mhofu database and inserts data into the mhofu table.
+"""
+
 import psycopg2
 
 # Establish a connection to the database
-conn = psycopg2.connect(database="mhofu", user="postgres", password="Eureka22", host="localhost", port="5432")
+conn=psycopg2.connect(database="mhofu", 
+                      user="postgres",
+                     password="Eureka22", 
+                     host="localhost",
+                     port="5432")
 
 # Create a cursor object
 cur = conn.cursor()
 
 # Execute an INSERT statement
-cur.execute("INSERT INTO mhofu(name, price) VALUES (CURRENT_DATE, %s)",  (0,))
+cur.execute("INSERT INTO mhofu(name, price) VALUES (CURRENT_DATE, %s)",
+            (0,))
 
 # Commit your changes to the database
 conn.commit()
@@ -15,3 +24,7 @@ conn.commit()
 # Close the cursor and connection
 cur.close()
 conn.close()
+
+#adding newlinecharacter 
+print()
+
