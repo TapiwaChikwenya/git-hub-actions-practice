7c4f8adfd47393de47a61234e697773f6a1b76a0
tapiwa <tapiwachikwenya@gmail.com>
2023-06-19 22:57:16 -0500

    fixed syntax

--- postgresinsert.py
+++ postgresinsert.py
@@ -1,13 +1,13 @@
 import psycopg2
 
 # Establish a connection to the database
-conn = psycopg2.connect(database="Mhofu", user="postgres", password="6809", host="localhost", port="5432")
+conn = psycopg2.connect(database="mhofu", user="postgres", password="Eureka22", host="localhost", port="5432")
 
 # Create a cursor object
 cur = conn.cursor()
 
 # Execute an INSERT statement
-cur.execute("INSERT INTO Mhofu(name, price) VALUES (%s, %s)",  ( SELECT CURRENT_DATE, 0))
+cur.execute("INSERT INTO mhofu(name, price) VALUES (CURRENT_DATE, %s)",  (0,))
 
 # Commit your changes to the database
 conn.commit()
